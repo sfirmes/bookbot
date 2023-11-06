11d75a461a3af450e3e0f22330e99e27f88f3e5e
sfirmes <steve@stevefirmes.com>
2023-11-06 13:34:47 -0500

    added code to count the number of words

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-#print("Hello World")
-with open("books/frankenstein.txt") as book:
-    print(book.read())
+def main():
+    book_path = "books/frankenstein.txt"
+    book_text = get_book_text(book_path)
+    print(count_words(book_text))
+
+def get_book_text(path):
+    with open(path) as book:
+        return book.read()
+    
+def count_words(text_line):
+    words = text_line.split()
+    return(len(words))
+
+main()
